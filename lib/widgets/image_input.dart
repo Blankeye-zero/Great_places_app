f238ce314f0110647eec5924c554eb2bde504ecc
Blankeye-zero <60312301+Blankeye-zero@users.noreply.github.com>
2023-03-12 17:15:15 +0530

    sqlite init local storage

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -15,6 +15,9 @@ class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
   Future<void> _takePicture () async{
     final XFile? imageFile = await ImagePicker().pickImage(source: ImageSource.camera, maxWidth: 600);
+    if(imageFile == null){
+      return;
+    }
     setState(() {
        _storedImage = File(imageFile!.path); 
     });
